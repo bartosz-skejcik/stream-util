61bd80c0aa57b9db98c34f63858c70ef126aed53
Bartłomiej Paczesny <bartek@paczesny.pl>
2025-02-26 13:53:21 +0100

    fix: move api url to env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ApiService, Response } from "@/types/services/api";
 
-const API_URL = "http://localhost:42069/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
